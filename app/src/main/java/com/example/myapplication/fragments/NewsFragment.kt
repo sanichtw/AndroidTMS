5b24e370e6ac2dd09601c8870f30a9aeadd2af38
Nikita Kozlov <sanichtw@gmail.com>
2023-04-24 20:02:34 +0300

    init new branch

--- app/src/main/java/com/example/myapplication/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/NewsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
@@ -27,36 +26,8 @@ class NewsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // initialize the spinner and the listView
-        initSpinner()
-        initListView()
-
         binding.nextButton.setOnClickListener {
             findNavController().navigate(R.id.action_NewsFragment_to_RecyclerViewFragment)
         }
-    }
-
-    private fun initSpinner() {
-        // creating an adapter
-        val adapter = ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.spinner_services,
-            android.R.layout.simple_spinner_item
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        // connecting the adapter and the spinner
-        binding.spinner.adapter = adapter
-    }
-
-    private fun initListView() {
-        // creating an adapter
-        val adapter = ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.listView_services,
-            android.R.layout.simple_list_item_1
-        )
-        // connecting the adapter and the listView
-        binding.listView.adapter = adapter
     }
 }
