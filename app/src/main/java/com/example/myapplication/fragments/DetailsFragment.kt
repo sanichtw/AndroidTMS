450b4562d83a745676a22ad0a029098f7d0ebe61
Nikita Kozlov <sanichtw@gmail.com>
2023-04-27 20:23:38 +0300

    did my homework05

--- app/src/main/java/com/example/myapplication/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/DetailsFragment.kt
@@ -5,14 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backButton.setOnClickListener {
+                findNavController().navigate(R.id.action_DetailsFragment_to_PaymentsFragment)
+        }
     }
 }
