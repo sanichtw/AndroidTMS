516d6af0d1997c15804cd70d79c0a7bbd39cf8fb
Nikita Kozlov <sanichtw@gmail.com>
2023-05-12 15:34:15 +0300

    initial commit

---
+++ app/src/main/java/com/example/myapplication/data/remote/api/PaymentsApi.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.data.remote.api
+
+interface PaymentsApi {
+    companion object {
+        const val GET_POSTS_REQUEST = "request_body"
+        const val KEY_PARAM = "key_parameter"
+    }
+}
